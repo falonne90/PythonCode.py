8efc8ae877428c599c29bad80c8fef04e591802f
falonne90 <falonnengass@gmail.com>
2022-11-11 03:02:40 -0800

    this program split, reverse and join

---
+++ test.py
@@ -0,0 +1,59 @@
+# this write spit, join and revert method
+
+print("Program to write Ssplit(),join() and reverse() methods on Python\n\n")
+
+word= input("\n what are your favorites colors\n\n") 
+
+# split method
+output= word.split()
+print(output)   
+
+# revert method
+revert= output.reverse()   
+print(output) 
+
+# join method
+list= ['blue', 'green', 'red', 'yellow']
+print(' '.join(list))
+
+
+
+
+
+
+
+"""
+print("fun code to draw a rectangle\n\n") 
+print("----------------------")
+print("|                    |")
+print("|                    |")
+print("----------------------")
+"""
+
+"""
+print("my first name")
+print("") # this option allow the space between the first and the second line
+print("my last name")
+"""
+
+"""
+print("Fun code to do add")
+print(" ")
+print(" ")
+a = "Mr Emma "  # this a is variable 1 where I assigned value "Mr Emma"
+b= "is a good man"  # variable b or variable 2 where I assigned value "is a good man"
+c= a+b      # variable c is an Expression to add the values of the variable a and b
+
+print(c)   # Display the result on the screen
+"""
+
+
+# I dont want the computer to read this line of code
+"""
+print("hello world")
+print("my morning routine starts at 6am")
+print("Practice makes perfect")
+print("The West Coast, the best Coast.")
+
+"""
+
