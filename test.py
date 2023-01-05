b2c40aabd498354b8e8a612f0beb65af2a9e1de1
falonne90 <98791001+falonne90@users.noreply.github.com>
2023-01-05 06:36:32 -0800

    Update test.py

--- test.py
+++ test.py
@@ -30,30 +30,4 @@ print("|                    |")
 print("----------------------")
 """
 
-"""
-print("my first name")
-print("") # this option allow the space between the first and the second line
-print("my last name")
-"""
-
-"""
-print("Fun code to do add")
-print(" ")
-print(" ")
-a = "Mr Emma "  # this a is variable 1 where I assigned value "Mr Emma"
-b= "is a good man"  # variable b or variable 2 where I assigned value "is a good man"
-c= a+b      # variable c is an Expression to add the values of the variable a and b
-
-print(c)   # Display the result on the screen
-"""
-
-
-# I dont want the computer to read this line of code
-"""
-print("hello world")
-print("my morning routine starts at 6am")
-print("Practice makes perfect")
-print("The West Coast, the best Coast.")
-
-"""
 
