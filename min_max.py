5318f05acc9c3fd0b36c708bfc7d5dd82562033f
falonne90 <falonnengass@gmail.com>
2023-01-05 06:20:12 -0800

    this python code print the minimum and maximum in a list of numbers.

---
+++ min_max.py
@@ -0,0 +1,8 @@
+
+# this is a python program that print the minimum and the maximum in a list of numbers.
+numbers= [1962, 2895, 325, 486,  5584,  656, 758,  8762,  901]  
+empty_list= []
+print(f"{max(numbers)}, {min(numbers)}")
+
+if len (empty_list) == 0:
+  print ("none")
---
+++ reverse.py
@@ -0,0 +1,16 @@
+
+# this program write spit, join and revert method
+print("Program to write Ssplit(),join() and reverse() methods on Python\n\n")
+word= input("\n what are your favorites colors\n\n") 
+
+# split method
+output= word.split()
+print(output)   
+
+# revert method
+output.reverse()   
+print(output) 
+
+# join method
+list= ['blue', 'green', 'red', 'yellow']
+print(' '.join(list))
